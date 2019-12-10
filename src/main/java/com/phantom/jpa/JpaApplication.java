b0c8045aa831ce6e12a18b114214db703db6e061
phantom <1342148007@qq.com>
2019-12-10 10:58:30 +0800

    修改测试，第一次

--- src/main/java/com/phantom/jpa/JpaApplication.java
+++ src/main/java/com/phantom/jpa/JpaApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JpaApplication {
 
+    //开发提交测试
     public static void main(String[] args) {
         SpringApplication.run(JpaApplication.class, args);
     }
